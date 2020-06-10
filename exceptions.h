d44aa445bbffe959f5527e2cb38ffdc44aa1a4f6
PatrykBujna <patrykbujna@macbook-pro-patryk.home>
2020-06-10 12:03:46 +0200

    Exceptions v0.2

--- exceptions.h
+++ exceptions.h
@@ -1,3 +1,4 @@
+#include <vector>
 #include <iostream>
 
 double *fun_1(int n)
@@ -5,12 +6,32 @@ double *fun_1(int n)
     try
     {
         double arr[n];
-        double *p = &arr[0];
-        return p;
+        return &arr[0];
     }
     catch (std::exception &e)
     {
         std::cerr << "Something went wrong: " << e.what() << std::endl;
         return NULL;
+    }
+}
+
+void replace(std::vector<int> a, std::vector<int> b, std::vector<int> c, int a1, int b1, int c1)
+{
+    if (a.size() - 1 >= a1 && b.size() - 1 >= b1 && c.size() - 1 >= c1)
+    {
+        int tmp1 = a[a1];
+        int tmp2 = b[b1];
+        int tmp3 = c[c1];
+        a[a1] = tmp3;
+        b[b1] = tmp1;
+        c[c1] = tmp2;
+        for (int n : a)
+        {
+            std::cout << n << std::endl;
+        }
+    }
+    else
+    {
+        throw std::out_of_range("Index is higher than size of vector!");
     }
 }
